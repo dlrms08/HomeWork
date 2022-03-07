01e59fe036335a5f8630cbaa7a0ebfec274b2314
Gon Lee <gon08@naver.com>
2022-03-08 07:04:22 +0900

    v0.1.4
    기본코드 전면 수정

--- Assets/2_Script/BoardManager.cs
+++ Assets/2_Script/BoardManager.cs
@@ -12,19 +12,12 @@ public class BoardManager : MonoBehaviour
 
     public Dot dotPrefab;
     public Transform dotsParent;
-    public List<Dot> dots = new List<Dot>();
-    public List<Dot> matchDots = new List<Dot>();
-    List<Dot> matchBank = new List<Dot>();
+    public GameObject[,] allDots;
 
     void Start()
     {
+        allDots = new GameObject[boardSize.x, boardSize.y];
         BoardInit();
-        while(true)
-        {
-            DotsInit();
-            if (matchBank.Count == 0) break;
-            ClearBoard();
-        }
     }
 
     private void Update()
@@ -55,7 +48,15 @@ public class BoardManager : MonoBehaviour
                 board.pos = pos;
                 board.transform.SetParent(transform, false);
                 board.transform.position = GetPosition(pos);
+                board.name = "(" + x + ", " + y + ")";
                 boards.Add(board);
+
+                var dot = Instantiate(dotPrefab, GetPosition(pos), Quaternion.identity);
+                dot.transform.SetParent(dotsParent, false);
+                dot.name = "(" + x + ", " + y + ")";
+                dot.column = x;
+                dot.row = y;
+                allDots[x, y] = dot.gameObject;
             }
         }
     }
@@ -65,111 +66,11 @@ public class BoardManager : MonoBehaviour
     {
         for(int i = 0; i < boards.Count; i++)
         {
-            var dot = Instantiate(dotPrefab);
-            dot.pos = boards[i].pos;
-            dot.transform.SetParent(dotsParent, false);
-            dot.transform.position = boards[i].transform.position;
-            dots.Add(dot);
-        }
-
-        CheckMatch();
-        if (matchDots.Count > 0)
-            ClearBoard();
-    }
-
-    //보드 삭제 
-    void ClearBoard()
-    {
-        for(int i = 0; i < dots.Count ; i++)
-        {
-            Destroy(dots[i].gameObject);
-        }
-        dots.Clear();
-        matchDots.Clear();
-        matchBank.Clear();
-
-        DotsInit();
-    }
-
-    //매치 클리어
-    public void ClearMatch()
-    {
-        foreach(var dot in matchDots)
-        {
-            dot.gameObject.SetActive(false);
-        }
-        matchDots.Clear();
-        matchBank.Clear();
-    }
-
-    //라인 매치확인
-    public void CheckMatch()
-    {
-        foreach(var dot in dots)
-        {
-            LineMatch(dot);
-        }
-        matchDots = matchBank.Distinct().ToList();
-    }
-
-    //직선매치 확인
-    public void LineMatch(Dot dot)
-    {
-        foreach (var it in Enum.GetValues(typeof(Direction)))
-        {
-            Direction dir = (Direction)it;
-            if (dir == Direction.None)
-                continue;
             
-            var neiver = FindDotObject(dot, dir);
-            if (neiver == null)
-                continue;
-
-            if (dot.dotType == neiver.dotType)
-            {
-                var neiver2 = FindDotObject(neiver, dir);
-                if (neiver2 == null)
-                    return;
-
-                if (neiver.dotType == neiver2.dotType)
-                {
-                    Debug.Log(dot.pos + " 와(과) " + neiver.pos + " 와(과) " + neiver2.pos + " 는 매치");
-                    matchBank.Add(dot);
-                    matchBank.Add(neiver);
-                    matchBank.Add(neiver2);
-                }
-            }
         }
     }
 
-    //라인 매치된 블록 찾기
-    Dot FindDotObject(Dot origin, Direction dir)
-    {
-        for(int i = 0; i < dots.Count; i++)
-        {
-            Vector2Int pos = GetNeighbor(origin.pos, dir);
-            if (pos == null)
-                continue;
-
-            if (dots[i].pos == pos)
-            {
-                return dots[i];
-            }
-        }
-        return null;
-    }
-
-    //보드의 빈 공간 찾기 
-    void CheckBlink()
-    {
-        for(int i = 0; i < boards.Count; i++)
-        {
-            if(!dots[i].gameObject.activeSelf)
-            {
-                Debug.Log(dots[i].pos);
-            }
-        }
-    }
+  
 
     //블록간 거리 계산 
     public Vector3 GetPosition(Vector2Int pos)
@@ -198,24 +99,5 @@ public class BoardManager : MonoBehaviour
                 return new Vector2Int(origin.x + 1, origin.y - 1);
         }
         return Vector2Int.zero;
-    }
-
-    public Direction GetDirection(Vector3 startPos, Vector3 endPos)
-    {
-        float degree = GetDirectionDegree(startPos, endPos);
-        if (degree <= 60f) return Direction.RightUp;
-        if (degree <= 120f) return Direction.Up;
-        if (degree <= 180f) return Direction.LeftUp;
-        if (degree <= 240f) return Direction.LeftDown;
-        if (degree <= 300f) return Direction.Down;
-        return Direction.RightDown;
-    }
-
-    public static float GetDirectionDegree(Vector3 origin, Vector3 lookAt)
-    {
-        Vector3 direction = lookAt - origin;
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        if (angle < 0f) angle += 360f;
-        return angle;
     }
 }
--- Assets/2_Script/Dot.cs
+++ Assets/2_Script/Dot.cs
@@ -4,30 +4,29 @@ using UnityEngine;
 
 public class Dot : MonoBehaviour
 {
-    public enum MoveType
-    {
-        None, Goal, Follow
-    }
-
-    public MoveType moveType;
 
     public DotType dotType = DotType.Top;
     public Color[] colors;
     private SpriteRenderer renderer;
 
-    public float stopDistance = 0.1f;
-    public Vector2Int pos;
-    public Vector3 goalPos;
+    public int column;
+    public int row;
+    public float targetX;
+    public float targetY;
+    private GameObject otherDot;
+    private BoardManager boardManager; 
+    private Vector2 firstTouchPosition;
+    private Vector2 finaltouchPosition;
+    private Vector2 tempPosition;
+    public float swipeAngle = 0;
 
     private void Awake()
     {
         renderer = GetComponent<SpriteRenderer>();
-
-    }
-
-    public bool IsMoving()
-    {
-        return moveType != MoveType.None;
+        boardManager = FindObjectOfType<BoardManager>();
+        Vector3 realpos = boardManager.GetPosition(new Vector2Int(column, row));
+        targetX = realpos.x;
+        targetY = realpos.y;
     }
 
     private void OnEnable()
@@ -37,22 +36,107 @@ public class Dot : MonoBehaviour
         renderer.color = colors[rnd];
     }
 
-    public void Moving(Vector3 pos)
+    private void Update()
     {
-        goalPos = pos;
-        moveType = MoveType.Goal;
-    }
-    
-    // Update is called once per frame
-    void Update()
-    {
-        if (moveType == MoveType.None) return;
+        Vector3 realpos = boardManager.GetPosition(new Vector2Int(column, row));
 
-        if (moveType == MoveType.Goal)
+        targetX = realpos.x;
+        targetY = realpos.y;
+        
+        if(Mathf.Abs(targetX - transform.position.x) > .1)
         {
-            transform.position = Vector3.MoveTowards(transform.position, goalPos, Global.dotSwapSpeed * Time.deltaTime);
-            if (Vector3.Distance(transform.position, goalPos) < stopDistance)
-                moveType = MoveType.None;
+            tempPosition = new Vector2(targetX, transform.position.y);
+            transform.position = Vector2.Lerp(transform.position, tempPosition, .4f);
+        }
+        else
+        {
+            tempPosition = new Vector2(targetX, transform.position.y);
+            transform.position = tempPosition;
+            boardManager.allDots[column, row] = this.gameObject;
+        }
+
+        if (Mathf.Abs(targetY - transform.position.y) > .1)
+        {
+            tempPosition = new Vector2(transform.position.x, targetY);
+            transform.position = Vector2.Lerp(transform.position, tempPosition, .4f);
+        }
+        else
+        {
+            tempPosition = new Vector2(transform.position.x, targetY);
+            transform.position = tempPosition;
+            boardManager.allDots[column, row] = this.gameObject;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        firstTouchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void OnMouseUp()
+    {
+        finaltouchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        CalculateAngle();
+    }
+
+    //드래그 앵글 계산.
+    private void CalculateAngle()
+    {
+        swipeAngle = Mathf.Atan2(finaltouchPosition.y - firstTouchPosition.y, finaltouchPosition.x - firstTouchPosition.x) * 180 / Mathf.PI;
+        MovePices();
+    }
+
+    void MovePices()
+    {
+        if (swipeAngle > 0 && swipeAngle <= 60 && column < boardManager.boardSize.x && row < boardManager.boardSize.y)
+        {
+            Debug.Log("오른쪽 위");
+            otherDot = boardManager.allDots[column + 1, row + 1];
+            otherDot.GetComponent<Dot>().column -= 1;
+            otherDot.GetComponent<Dot>().row -= 1;
+            column += 1;
+            row += 1;
+        }
+        else if (swipeAngle > 60 && swipeAngle <= 120 && row < boardManager.boardSize.y)
+        {
+            Debug.Log("위");
+            otherDot = boardManager.allDots[column, row + 2];
+            otherDot.GetComponent<Dot>().row -= 2;
+            row += 2;
+        }
+        else if (swipeAngle > 120 && swipeAngle < 180 && column > 0 && row < boardManager.boardSize.y)
+        {
+            Debug.Log("왼쪽 위");
+            otherDot = boardManager.allDots[column - 1, row + 1];
+            otherDot.GetComponent<Dot>().column += 1;
+            otherDot.GetComponent<Dot>().row -= 1;
+            column -= 1;
+            row += 1;
+        }
+        else if (swipeAngle > -180 && swipeAngle <= -120 && column > 0 && row > 0)
+        {
+            Debug.Log("왼쪽 아래");
+            otherDot = boardManager.allDots[column - 1, row - 1];
+            otherDot.GetComponent<Dot>().column += 1;
+            otherDot.GetComponent<Dot>().row += 1;
+            column -= 1;
+            row -= 1;
+        }
+        else if (swipeAngle > -120 && swipeAngle <= -60 && row > 0)
+        {
+            Debug.Log("아래");
+            otherDot = boardManager.allDots[column, row - 2];
+            otherDot.GetComponent<Dot>().row += 2;
+            row -= 2;
+        }
+        else if(swipeAngle > -60 && swipeAngle < 0 && column < boardManager.boardSize.x && row > 0)
+        {
+            Debug.Log("오른쪽 아래");
+            otherDot = boardManager.allDots[column + 1, row - 1];
+            otherDot.GetComponent<Dot>().column -= 1;
+            otherDot.GetComponent<Dot>().row += 1;
+            column += 1;
+            row -= 1;
         }
     }
 }
